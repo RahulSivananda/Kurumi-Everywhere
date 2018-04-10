4dfc4f3f383b291e607134b7be6d0fcf58e0660d
RahulSivananda <rahulhari94@gmail.com>
2018-04-11 00:34:50 +0530

    Kurumi chan is love, kurumi chan is life
    adding source code

---
+++ kurumi.js
@@ -0,0 +1,22 @@
+window.setInterval(myCallback, 100);
+function myCallback() {
+  kurumiChwaaaan();
+}
+let images=[]
+let path="images/tokisaki_kurumi_";
+let files=[];
+for(let i=1;i<8;i++){
+	files.push(i+'.jpg');
+}
+
+function kurumiChwaaaan(){
+	let tmp=document.getElementsByTagName('img');
+	for(let image of tmp){
+		if(images.indexOf(image)==-1){
+			images.push(image);
+			let index = Math.floor(Math.random()*files.length);
+			image.src = browser.extension.getURL(path+files[index]);
+		}
+	}
+}
+kurumiChwaaaan();
